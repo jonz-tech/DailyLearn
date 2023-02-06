db867527b9d9b770ad6d8122cdc152a8d6729fe2
jonz <jonz.tech@gmail.com>
2023-02-07 01:15:57 +0800

    swift daily: protocol

--- 大前端/iOS/swift/SwiftyDaily/SwiftyDaily/LanguageGuide/21_协议Protocol.playground/Contents.swift
+++ 大前端/iOS/swift/SwiftyDaily/SwiftyDaily/LanguageGuide/21_协议Protocol.playground/Contents.swift
@@ -74,12 +74,103 @@ class Starship: FullyNamed {
 var ncc1701 = Starship(name: "Enterprise", prefix: "USS") // ncc1701.fullName is "USS Enterprise"
 
 // ## 方法要求条件
-// 协议可以要求实现类型指定实例方法和类型方法
+// 协议可以要求实现类型指定实例方法和类型方法。这些方法的声明和普通的实例和类方法声明一样，但它没有{}和方法体。
+// 可以在协议中定义具有可变参数的方法，和普通方法的定义方式相同。但是，不支持为协议中的方法提供默认参数。
 
-// ## 可变方法要求条件
+// 1. 协议中的类方法：用static和class作为关键字前缀。
+ protocol StaticProtocol {
+     static func someTypeMethod()
+ }
 
+// 2. 实例方法和普通的方法声明一样。
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c).truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+// 打印 “Here's a random number: 0.37464991998171”
+print("And another one: \(generator.random())")
+// 打印 “And another one: 0.729023776863283”
+
+// ## 异变方法要求条件
+// 通常有些场景，我们需要用方法来改变实例属性或者实例本身，这个方法我们称为异变方法。
+// 把mutating关键字，放在func关键字前面表示异变方法。协议中如果也需要异变方法，同样声明mutating即可。
+protocol Togglable {
+    mutating func toggle()
+}
+// PS:
+// 1. 如果实现协议中的mutating 方法对象是类类型，则不用写mutating关键字。
+// 2. 如果实现协议异变方法的对象是结构体和枚举，则必须写mutating关键字
+enum OnOffSwitch: Togglable {
+    case off, on
+    mutating func toggle() {
+        switch self {
+        case .off:
+            self = .on
+        case .on:
+            self = .off
+        }
+    }
+}
+var lightSwitch = OnOffSwitch.off
+lightSwitch.toggle()   // lightSwitch 现在的值为 .on
 
 // ## 构造函数要求条件
+// 同样也可以在协议中，声明你想要的构造函数。声明方式和构造函数的定义一样，但没有{}和构造体。
+protocol SomeInitProtocol {
+    init(someParameter: Int)
+}
+
+// 实现协议的对象是类的话，无论是指定构造器，还是便利构造起，你都必须给构造器实现方法加上require关键字。
+// 好处是：可以确保所有子类也必须提供此构造器实现，从而也能遵循协议。
+class SomeInitClass: SomeInitProtocol {
+    required init(someParameter: Int) { //如果不声明require，这里会编译报错。
+        // initializer implementation goes here
+    }
+}
+
+// 如果类已经被标记为 final，那么不需要在协议构造器的实现中使用 required 修饰符。
+// 因为 final 类不能有子类。。
+final class FinalInitClass: SomeInitProtocol {
+    required init(someParameter: Int) { //这里require是可选项，可加可不加。
+        // initializer implementation goes here
+    }
+}
+
+// 如果一个类继承了父类和协议，并且协议都有init方法。那么需要把require和override写上。
+protocol SomeProtocol1 {
+    init()
+}
+
+class SomeSuperClass1 {
+    init() {
+        // initializer implementation goes here
+    }
+}
+
+class SomeSubClass: SomeSuperClass1, SomeProtocol1 {
+    // "required" from SomeProtocol conformance; "override" from SomeSuperClass
+    required override init() {
+        // initializer implementation goes here
+    }
+}
+
+// 可失败构造函数要求
+// 对于协议定义，你也可以声明可失败构造函数。
+// 1. 可失败构造器（init?）：可失败构造函数实现，可以是可失败的(返回nil)，或者不可失败的（不返回nil）。
+// 2. 非可失败构造器（init）：不可失败构造函数实现，可以上不可失败（不返回nil），或者有可失败构造函数强制转换表示(init!)
+
 
 
 // ## 协议作为类型
